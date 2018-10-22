49b3c286bc87d6cfae7ba8be1b331fc61e5403b3
Malik Harvey <malik.harvey@northlandcaps.org>
2018-10-22 08:18:14 -0500

    init git

---
+++ app/src/main/java/com/example/northlandcaps/crisis_response/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.northlandcaps.crisis_response;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button _locationlog,_groupchat,_settings,_usermanager,_buildingmanager,_roommanager,_logout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button _loginbutton = findViewById(R.id.loginbutton);
+        _loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginintent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(loginintent);
+            }
+        });
+    }
+}
