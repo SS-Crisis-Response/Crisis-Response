dffefd12348d8b2a9dfeec487683dd372328e4b9
casheridan <christian.sheridan@northlandcaps.org>
2018-10-25 15:32:09 -0500

    Changed settings title

--- app/src/main/java/com/example/northlandcaps/crisis_response/setingshandler.java
+++ app/src/main/java/com/example/northlandcaps/crisis_response/setingshandler.java
@@ -9,6 +9,6 @@ public class setingshandler extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setingshandler);
-        this.setTitle("User Management");
+        this.setTitle("Settings");
     }
 }
