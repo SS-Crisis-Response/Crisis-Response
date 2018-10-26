3aa3aaca42a262187cc78a26a7e2c30fa58b3f08
Malik Harvey <malik.harvey@northlandcaps.org>
2018-10-26 08:48:19 -0500

    wheeeeeeeeeeeeeeeeeeeeeeeeeee

--- app/src/main/java/com/example/northlandcaps/crisis_response/Global.java
+++ app/src/main/java/com/example/northlandcaps/crisis_response/Global.java
@@ -1,5 +1,8 @@
 package com.example.northlandcaps.crisis_response;
 
+import android.graphics.Color;
+
 public class Global {
     public static boolean active;
+    public int textcolors = Color.parseColor("FF000000");
 }
--- app/src/main/java/com/example/northlandcaps/crisis_response/setingshandler.java
+++ app/src/main/java/com/example/northlandcaps/crisis_response/setingshandler.java
@@ -1,7 +1,13 @@
 package com.example.northlandcaps.crisis_response;
 
+import android.graphics.Color;
+import android.graphics.ColorSpace;
+import android.graphics.drawable.GradientDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class setingshandler extends AppCompatActivity {
 
@@ -9,6 +15,38 @@ public class setingshandler extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setingshandler);
+
         this.setTitle("Settings");
+        SeekBar seekbar = findViewById(R.id.darknessbar);
+        final TextView progresstext = findViewById(R.id.progresstext);
+        final ConstraintLayout settings =findViewById(R.id.settingspage);
+        seekbar.setProgress(1);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekBar.setProgress(progress);
+                System.out.println(progress);
+                if (progress==1){
+                    int[] normalcolor = {Color.parseColor("#679acf"),Color.parseColor("#3b5e94")};
+                     GradientDrawable NormalGD = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, normalcolor);
+                    settings.setBackground(NormalGD);
+                }else if(progress ==0){
+                    int[] darkcolor = {Color.parseColor("#203453"),Color.parseColor("#436486")};
+                    GradientDrawable DarkGD = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, darkcolor);
+                    settings.setBackground(DarkGD);
+                    progresstext.setText("Dark Colors");
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
