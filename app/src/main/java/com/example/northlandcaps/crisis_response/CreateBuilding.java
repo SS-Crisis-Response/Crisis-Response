da70f0af946a16d4a069713187bcf4da96da2eb0
casheridan <christian.sheridan@northlandcaps.org>
2018-10-29 09:39:29 -0500

    Added the dark colors for the create building page

--- app/src/main/java/com/example/northlandcaps/crisis_response/CreateBuilding.java
+++ app/src/main/java/com/example/northlandcaps/crisis_response/CreateBuilding.java
@@ -2,6 +2,7 @@ package com.example.northlandcaps.crisis_response;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class CreateBuilding extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class CreateBuilding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_building);
+        Button CreateBuilding = findViewById(R.id.CreateBuilding2);
+
+        if (Global.themetype ==0){
+            CreateBuilding.setBackgroundResource(R.drawable.dark_menu_buttons);
+            CreateBuilding.setTextColor(Global.textdarkcolors);
+        }
+        else if (Global.themetype ==1){
+
+        }
+        else if (Global.themetype ==2){
+
+        }
     }
 }
