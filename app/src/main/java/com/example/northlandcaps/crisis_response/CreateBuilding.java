a3994303f17495f4aae8523142e7fd2845997df2
casheridan <christian.sheridan@northlandcaps.org>
2018-10-29 09:50:58 -0500

    Changed create building stuff

--- app/src/main/java/com/example/northlandcaps/crisis_response/CreateBuilding.java
+++ app/src/main/java/com/example/northlandcaps/crisis_response/CreateBuilding.java
@@ -10,17 +10,22 @@ public class CreateBuilding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_building);
-        Button CreateBuilding = findViewById(R.id.CreateBuilding2);
+        Button CreateBuilding = findViewById(R.id.AddBuilding);
 
         if (Global.themetype ==0){
+            CreateBuilding.setBackground(Global.DarkGD);
             CreateBuilding.setBackgroundResource(R.drawable.dark_menu_buttons);
             CreateBuilding.setTextColor(Global.textdarkcolors);
         }
         else if (Global.themetype ==1){
-
+            CreateBuilding.setBackground(Global.NormalGD);
+            CreateBuilding.setBackgroundResource(R.drawable.menu_buttons);
+            CreateBuilding.setTextColor(Global.textnormalcolors);
         }
         else if (Global.themetype ==2){
-
+            CreateBuilding.setBackground(Global.LightGD);
+            CreateBuilding.setBackgroundResource(R.drawable.light_menu_button);
+            CreateBuilding.setTextColor(Global.textlightcolors);
         }
     }
 }
