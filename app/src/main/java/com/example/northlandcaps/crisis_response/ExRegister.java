fd0c10c5cc67cb692cab6890834038d7ed88a03e
Malik Harvey <malik.harvey@northlandcaps.org>
2018-11-01 09:55:54 -0500

    expirementing

---
+++ app/src/main/java/com/example/northlandcaps/crisis_response/ExRegister.java
@@ -0,0 +1,13 @@
+package com.example.northlandcaps.crisis_response;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ExRegister {
+        try {
+            Class.forName("Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+}
