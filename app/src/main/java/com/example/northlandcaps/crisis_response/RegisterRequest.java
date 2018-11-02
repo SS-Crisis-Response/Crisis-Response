b9632136cbf2004f9e6b9c6d73c3290a9425751d
Malik Harvey <malik.harvey@northlandcaps.org>
2018-11-02 09:27:13 -0500

    trying to AAAAAAAAHHHHHH

--- app/src/main/java/com/example/northlandcaps/crisis_response/RegisterRequest.java
+++ app/src/main/java/com/example/northlandcaps/crisis_response/RegisterRequest.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class RegisterRequest extends StringRequest {
 
-    private static final String REGISTER_REQUEST_URL = "https://testingserverhost.000webhostapp.com/Register.php";
+    private static final String REGISTER_REQUEST_URL = "http://192.168.6.104:3306/phptesting/Register.php";
     private Map<String, String> params;
     public RegisterRequest(String username, String password,String isAdmin, Response.Listener<String> listener){
         super(Method.POST, REGISTER_REQUEST_URL,listener,null);
