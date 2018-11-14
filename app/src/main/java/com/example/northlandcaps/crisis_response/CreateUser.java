3ab9900dded35755950549d4a23e3b30b79a0fcb
Ashna Ali <ashna.ali@northlandcaps.org>
2018-11-14 08:55:09 -0600

    added call options page (someone please fix contraints)

--- app/src/main/java/com/example/northlandcaps/crisis_response/CreateUser.java
+++ app/src/main/java/com/example/northlandcaps/crisis_response/CreateUser.java
@@ -1,9 +1,6 @@
 package com.example.northlandcaps.crisis_response;
 
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -13,12 +10,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
-import android.widget.Toast;
+
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class CreateUser extends AppCompatActivity {
     @Override
@@ -27,7 +22,7 @@ public class CreateUser extends AppCompatActivity {
         setContentView(R.layout.activity_create_user);
         this.setTitle("Create User");
         final EditText username1 = findViewById(R.id.Createusername);
-        final EditText password1 = findViewById(R.id.CreatePassword);
+        final EditText password1 = findViewById(R.id.Createpassword);
         final Switch isAdmin = findViewById(R.id.isadmin);
         final Button createuser = findViewById(R.id.createuserbtn);
         if (getIntent().hasExtra("com.example.northlandcaps.crisis_response")){
@@ -68,7 +63,7 @@ public class CreateUser extends AppCompatActivity {
         super.onResume();
         final ConstraintLayout createuserpage = findViewById(R.id.createuser);
         EditText createusername = findViewById(R.id.Createusername);
-        EditText createpassword = findViewById(R.id.CreatePassword);
+        EditText createpassword = findViewById(R.id.Createpassword);
         Switch isAdmin = findViewById(R.id.isadmin);
         Button createuserbtn = findViewById(R.id.createuserbtn);
             if (Global.themetype==0){
