955c50a455700587e8b2ced2c1fa6dce4fcdbaee
Malik Harvey <malik.harvey@northlandcaps.org>
2018-12-13 07:32:12 -0600

    optimized Main Activity

--- app/src/main/java/com/example/northlandcaps/crisis_response/MainActivity.java
+++ app/src/main/java/com/example/northlandcaps/crisis_response/MainActivity.java
@@ -2,10 +2,9 @@ package com.example.northlandcaps.crisis_response;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,26 +12,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
-import com.google.firebase.database.FirebaseDatabase;
 import com.onesignal.OneSignal;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    private EditText email,password,name;
-    private Button signin,signup;
     FirebaseUser user;
     static String LoggedIn_Username;
 
@@ -62,12 +51,6 @@ public class MainActivity extends AppCompatActivity {
         final TextView registerlink = findViewById(R.id.registertxtv);
         registerlink.setTextColor(Color.rgb(0, 0, 0));
 
-        loginbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
